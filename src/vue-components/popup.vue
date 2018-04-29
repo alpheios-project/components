347fff3b811116ed0ca238c95524d911dce33eb7
irina060981 <irina060981@yandex.ru>
2018-04-29 10:28:19 +1000

    added styles alph_tooltip to styles folder and add test html tags

--- src/vue-components/popup.vue
+++ src/vue-components/popup.vue
@@ -9,10 +9,13 @@
                 <span v-show="data.status.selectedText" class="alpheios-popup__header-selection">{{data.status.selectedText}}</span>
                 <span v-show="data.status.languageName && data.verboseMode" class="alpheios-popup__header-word">({{data.status.languageName}})</span>
             </div>
-            <div class="uk-button-group alpheios-popup__button-area testtooltip">
-                <button @click="showPanelTab('inflections')" v-show="data.inflDataReady"
-                        class="uk-button uk-button-primary uk-button-small alpheios-popup__more-btn">{{data.l10n.messages.LABEL_POPUP_INFLECT}}
-                </button>
+            <div class="uk-button-group alpheios-popup__button-area">
+                <div class="alph_tooltip">
+                  <button @click="showPanelTab('inflections')" v-show="data.inflDataReady"
+                          class="uk-button uk-button-primary uk-button-small alpheios-popup__more-btn">{{data.l10n.messages.LABEL_POPUP_INFLECT}}
+                  </button>
+                  <span class="tooltiptext  alph_tooltip-top">Show inflections</span>
+                </div>
                 <button @click="showPanelTab('definitions')" v-show="data.defDataReady"
                         class="uk-button uk-button-primary uk-button-small alpheios-popup__more-btn">{{data.l10n.messages.LABEL_POPUP_DEFINE}}
                 </button>
@@ -500,6 +503,7 @@
 </script>
 <style lang="scss">
     @import "../styles/alpheios";
+    @import "../styles/tooltip";
 
     .alpheios-popup {
         display: flex;
