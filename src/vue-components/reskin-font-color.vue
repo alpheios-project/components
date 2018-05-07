8e872103e88d886c0d65fc05ea04c15a0cc4e82d
irina060981 <irina060981@yandex.ru>
2018-05-07 19:15:01 +1000

    style fixes

--- src/vue-components/reskin-font-color.vue
+++ src/vue-components/reskin-font-color.vue
@@ -22,6 +22,9 @@
     .alpheios-skin_panel {
     	display: inline-block;
     }
+
+    $baseSize: 22px;
+
     .alpheios-skin_properties {
     	margin: 0;
     	padding: 0;
@@ -31,20 +34,28 @@
     		display: inline-block;
     		font-size: 16px;
 
-    		width: 20px;
-    		height: 20px;
+    		width: $baseSize;
+    		height: $baseSize;
     		text-align: center;
-    		line-height: 20px;
+    		line-height: $baseSize;
 
     		color: #fff;
     		background: #27323b;
-    		border-radius: 10px;
+    		border-radius: $baseSize/2;
 
     		&.alpheios-small_font {
     			font-size: 80%;
+                width: $baseSize * 0.8;
+                height: $baseSize * 0.8;
+                line-height: $baseSize * 0.8;
+                border-radius: $baseSize/2  * 0.8;
     		}
     		&.alpheios-large_font {
     			font-size: 120%;
+                width: $baseSize * 1.2;
+                height: $baseSize * 1.2;
+                line-height: $baseSize * 1.2;
+                border-radius: $baseSize/2  * 1.2;
     		}
     	}
     }
