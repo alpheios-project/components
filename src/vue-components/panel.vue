e95e28ebd350e75c872f26c5b5e534169ecc54ee
irina060981 <irina060981@yandex.ru>
2018-05-07 20:45:04 +1000

    fixes

--- src/vue-components/panel.vue
+++ src/vue-components/panel.vue
@@ -232,56 +232,23 @@
     computed: {
       classes: function () {
         // Add font size class
-        let vm = this
+        // let vm = this
 
-        Object.keys(this.fontSizeClassVariants).forEach(function(type) {
-          let index = vm.data.classes.findIndex(v => v === vm.fontSizeClassVariants[type])
-          if (index >= 0) { delete vm.data.classes[index] }
-        })
-        this.data.classes.push(this.fontSizeClassVariants[this.currentFontSizeType])
-
-        Object.keys(this.positionClassVariants).forEach(function(type) {
-          let index = vm.data.classes.findIndex(v => v === vm.positionClassVariants[type])
-          if (index >= 0) { delete vm.data.classes[index] }
-        })
-        this.data.classes.push(this.positionClassVariants[this.data.settings.panelPosition.currentValue])
+        // Object.keys(this.fontSizeClassVariants).forEach(function(type) {
+        //   let index = vm.data.classes.findIndex(v => v === vm.fontSizeClassVariants[type])
+        //   if (index >= 0) { delete vm.data.classes[index] }
+        // })
+        // this.data.classes.push(this.fontSizeClassVariants[this.currentFontSizeType])
 
         // Find index of an existing position class and replace it with an updated value
-        // const positionLeftIndex = this.data.classes.findIndex(v => v === this.positionLeftClassName)
-        // const positionRightIndex = this.data.classes.findIndex(v => v === this.positionRightClassName)
 
-        // if (positionRightIndex >= 0) { delete vm.data.classes[positionRightIndex] }
-        // if (positionLeftIndex >= 0) { delete vm.data.classes[positionLeftIndex] }
-
-        // if (this.data.settings.panelPosition.currentValue === 'left') {
-        //   this.data.classes.push(this.positionLeftClassName)
-        // }
-        // if (this.data.settings.panelPosition.currentValue === 'right') {
-        //   this.data.classes.push(this.positionRightClassName)
-        // }   
-
-
-
-
-        // if (this.data.settings.panelPosition.currentValue === 'left') {
-        //   if (positionRightIndex >= 0) {
-        //     // Replace an existing value
-        //     this.data.classes[positionRightIndex] = this.positionLeftClassName
-        //   } else {
-        //     // Add an initial value
-        //     this.data.classes.push(this.positionLeftClassName)
-        //   }
-
-        // } else if (this.data.settings.panelPosition.currentValue === 'right') {
-        //   if (positionLeftIndex >= 0) {
-        //     // Replace an existing value
-        //     this.data.classes[positionLeftIndex] = this.positionRightClassName
-        //   } else {
-        //     // Add an initial value
-        //     this.data.classes.push(this.positionRightClassName)
-        //   }
-        // }
-       
+        // Object.keys(this.positionClassVariants).forEach(function(type) {
+        //   let index = vm.data.classes.findIndex(v => v === vm.positionClassVariants[type])
+        //   if (index >= 0) { delete vm.data.classes[index] }
+        // })
+        // this.data.classes.push(this.positionClassVariants[this.data.settings.panelPosition.currentValue])      
+        this.updateClasses('fontSizeClassVariants', this.currentFontSizeType)
+        this.updateClasses('positionClassVariants', this.data.settings.panelPosition.currentValue)
         return this.data.classes
       },
 
@@ -325,6 +292,14 @@
       }
     },
     methods: {
+      updateClasses: function (classGroup, currentValue) {
+        let vm = this
+        Object.keys(this[classGroup]).forEach(function(type) {
+          let index = vm.data.classes.findIndex(v => v === vm[classGroup][type])
+          if (index >= 0) { delete vm.data.classes[index] }
+        })
+        this.data.classes.push(this.classGroup[currentValue])      
+      },
       updateZIndex: function (zIndexMax) {
         if (zIndexMax >= this.zIndex) {
           this.zIndex = zIndexMax
