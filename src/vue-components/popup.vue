9943a3b0d18cb910c355bb99bb08d557b25de0ec
irina060981 <irina060981@yandex.ru>
2018-05-10 00:00:33 +1000

    updateDimensions fixes

--- src/vue-components/popup.vue
+++ src/vue-components/popup.vue
@@ -440,15 +440,11 @@
        */
       updatePopupDimensions () {
         let time = new Date().getTime()
-        console.log('***********updatePopupDimensions 1', this.heightDm)
-        if (this.heightDm !== 'auto') { 
+        if (this.heightDm !== 'auto' && this.$el.scrollHeight > this.$el.offsetHeight) { 
           this.heightDm ='auto' 
-          console.log('*************updatePopupDimensions 2')
         }
-        console.log('***********updatePopupDimensions 3', this.heightDm)
         if (this.resizeCount >= this.resizeCountMax) {
           // Skip resizing if maximum number reached to avoid infinite loops
-          console.log('***********resizeCount', this.resizeCount, this.resizeCountMax)
           return
         }
 
