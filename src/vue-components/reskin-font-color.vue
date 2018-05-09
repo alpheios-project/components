cc0e9f14c9fe35a7a2789b5761dfb5e58c4c0726
irina060981 <irina060981@yandex.ru>
2018-05-09 20:02:05 +1000

    adapting popup buttons

--- src/vue-components/reskin-font-color.vue
+++ src/vue-components/reskin-font-color.vue
@@ -28,9 +28,7 @@
     	display: inline-block;
     }
 
-    $smallSize: $alpheios-small-font-size + 2*6px;
-    $baseSize: $alpheios-base-font-size + 2*6px;
-    $largeSize: $alpheios-large-font-size + 2*6px;
+    $baseSize: $alpheios-base-font-size + 12px;
 
     .alpheios-skin_panel ul.alpheios-skin_properties {
     	margin: 0;
@@ -54,18 +52,18 @@
             cursor: pointer;
 
     		&.alpheios-small_font {
-    			font-size: $alpheios-small-font-size;
-                width: $smallSize;
-                height: $smallSize;
-                line-height: $smallSize;
-                border-radius: $smallSize;
+    			font-size: $alpheios-base-font-size * 0.8;
+                width: $baseSize * 0.8;
+                height: $baseSize * 0.8;
+                line-height: $baseSize * 0.8;
+                border-radius: $baseSize * 0.8 * 0.5;
     		}
     		&.alpheios-large_font {
-    			font-size: $alpheios-large-font-size;
-                width: $largeSize;
-                height: $largeSize;
-                line-height: $largeSize;
-                border-radius: $largeSize;
+    			font-size:$alpheios-base-font-size * 1.2;
+                width: $baseSize * 1.2;
+                height: $baseSize * 1.2;
+                line-height: $baseSize * 1.2;
+                border-radius: $baseSize * 1.2;
     		}
     	}
     }
