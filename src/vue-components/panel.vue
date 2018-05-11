01e069290dd697503633c4701594bae4e59020d0
irina060981 <irina060981@yandex.ru>
2018-05-11 17:21:56 +1000

    style lookup on the panel

--- src/vue-components/panel.vue
+++ src/vue-components/panel.vue
@@ -83,8 +83,8 @@
               </alph-tooltip>
             </span>
         </div>
-        <div class="alpheios-panel__header" style="grid-area: c; display:none;">for reskin panel</div>
-        <div class="alpheios-panel__header" style="grid-area: d;"><lookup :uiController="uiController"></lookup></div>
+        <div class="alpheios-panel__header" style="grid-area: b; display:none;">for reskin panel</div>
+        <div class="alpheios-panel__header" style="grid-area: b;"><lookup :uiController="uiController"></lookup></div>
 
         <div class="alpheios-panel__content">
             <div v-show="data.tabs.definitions" class="alpheios-panel__tab-panel">
