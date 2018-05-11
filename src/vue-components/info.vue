00430e7c4d4cbfb50cbacc32eeb9febe4679ca1f
irina060981 <irina060981@yandex.ru>
2018-05-11 13:29:36 +1000

    panel color schema

--- src/vue-components/info.vue
+++ src/vue-components/info.vue
@@ -35,14 +35,14 @@
   .alpheios-info {
     font-family: $alpheios-font-family !important;
     font-size: $alpheios-base-font-size !important;
-    color: $alpheios-copy-color !important;
+    color: $alpheios-copy-color;
   }
 
   .alpheios-info .alpheios-info__helptext p {
     /*font-size: 0.875*$alpheios-base-font-size;*/
     font-family: $alpheios-font-family !important;
     font-size: $alpheios-base-font-size;
-    color: $alpheios-copy-color !important;
+    color: $alpheios-copy-color;
   }
 
   .alpheios-info .alpheios-info__versiontext {
