647c7ba7809d5bf9196d8e7be94de52d4069d82a
irina060981 <irina060981@yandex.ru>
2018-05-13 11:43:48 +1000

    fixes

--- src/vue-components/panel.vue
+++ src/vue-components/panel.vue
@@ -478,11 +478,11 @@
         border-bottom: 1px solid $alpheios-link-color-dark-bg;
     }
 
-    .alpheios-panel__header .reskin_panel {
-      grid-area: "title";
+    .alpheios-panel__header.reskin_panel {
+      grid-area: title;
     }
-    .alpheios-panel__header .lookup_panel {
-      grid-area: "lookup";
+    .alpheios-panel__header.lookup_panel {
+      grid-area: lookup;
     }
     .alpheios-panel-left .alpheios-panel__header {
         direction: ltr;
