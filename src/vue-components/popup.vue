0be176a32e1b179e1a18c22e6fa50e6b575d1123
irina060981 <irina060981@yandex.ru>
2018-05-14 20:46:16 +1000

    remove unuseful classes

--- src/vue-components/popup.vue
+++ src/vue-components/popup.vue
@@ -1,5 +1,5 @@
 <template>
-    <div ref="popup" class="irina-test alpheios-popup auk" v-bind:class="data.classes" :style="{left: positionLeftDm, top: positionTopDm, width: widthDm, height: heightDm}"
+    <div ref="popup" class="alpheios-popup auk" v-bind:class="data.classes" :style="{left: positionLeftDm, top: positionTopDm, width: widthDm, height: heightDm}"
          v-show="visible" :data-notification-visible="data.notification.visible">
          <alph-tooltip
           tooltipDirection = "left"
