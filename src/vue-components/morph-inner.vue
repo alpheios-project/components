ae99c5fb89e96710df98bb3d22e20c0d44524255
irina060981 <irina060981@yandex.ru>
2018-05-14 22:03:12 +1000

    research for the error in morph-inner

--- src/vue-components/morph-inner.vue
+++ src/vue-components/morph-inner.vue
@@ -60,7 +60,8 @@
                     <inflectionattribute :data="nextGroup.groupingKey" :type="types.tense" :linkedfeatures="linkedfeatures" :grouplevel="3" @sendfeature="sendFeature"/>
                     <inflectionattribute :data="nextGroup.groupingKey" :type="types.voice" :linkedfeatures="linkedfeatures" :grouplevel="3" @sendfeature="sendFeature"/>
                   </span>
-                  <span class="alpheios-morph__groupseparator" v-if="group.groupingKey && group.groupingKey.isCaseInflectionSet && (group.groupingKey[types.number] || group.groupingKey && group.groupingKey[types.tense] || nextGroup.groupingKey[types.tense] || nextgroup.groupingKey[types.voice])">:</span>
+                  {{ group.groupingKey }}
+                  <!-- <span class="alpheios-morph__groupseparator" v-if="group.groupingKey && group.groupingKey.isCaseInflectionSet && (group.groupingKey[types.number] || group.groupingKey && group.groupingKey[types.tense] || nextGroup.groupingKey[types.tense] || nextgroup.groupingKey[types.voice])">:</span> -->
                   
                   <div v-for="infl in nextGroup.inflections"
                     :class="groupClass(nextGroup)">
