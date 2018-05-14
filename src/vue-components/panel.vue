3b7d6016d11a6ca860aee55cf4f169ca96c03ed7
irina060981 <irina060981@yandex.ru>
2018-05-14 20:32:08 +1000

    remove inline styles fixes

--- src/vue-components/panel.vue
+++ src/vue-components/panel.vue
@@ -83,8 +83,8 @@
               </alph-tooltip>
             </span>
         </div>
-        <div class="alpheios-panel__header">for reskin panel</div>
-        <div class="alpheios-panel__header"><lookup :uiController="uiController"></lookup></div>
+        <div class="alpheios-panel__header reskin_panel">for reskin panel</div>
+        <div class="alpheios-panel__header lookup_panel"><lookup :uiController="uiController"></lookup></div>
 
         <div class="alpheios-panel__content">
             <div v-show="data.tabs.definitions" class="alpheios-panel__tab-panel">
