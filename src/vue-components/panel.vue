3bfe7928e10c437752614fe1915fb4de774ea1fa
irina060981 <irina060981@yandex.ru>
2018-05-14 20:40:14 +1000

    remove inline styles fixes

--- src/vue-components/panel.vue
+++ src/vue-components/panel.vue
@@ -418,7 +418,7 @@
         direction: ltr;
         display: grid;
         grid-template-columns: auto;
-        grid-template-rows: #{$alpheios-panel-header-height} #{$alpheios-panel-title-height} #{$alpheios-panel-title-height} auto  60px;
+        grid-template-rows: #{$alpheios-panel-header-height} 0 #{$alpheios-panel-title-height} auto  60px;
         grid-template-areas:
             "header"
             "title"
