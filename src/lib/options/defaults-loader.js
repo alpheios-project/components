0d1642956aa3a33be116417766b1fad238b9f76a
irina060981 <irina060981@yandex.ru>
2018-05-15 23:27:56 +1000

    fixes

--- src/lib/options/defaults-loader.js
+++ src/lib/options/defaults-loader.js
@@ -1,7 +1,6 @@
 export default class DefaultsLoader {
   static fromJSON (jsonString) {
     try {
-      console.log('**********jsonString', jsonString)
       return JSON.parse(jsonString)
     } catch (err) {
       console.error(`Unable to parse JSON options string:`, err)
