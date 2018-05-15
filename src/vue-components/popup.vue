2a7e48f61f62f84d5dc39ba09b353ff40b2b66ef
irina060981 <irina060981@yandex.ru>
2018-05-15 13:14:50 +1000

    fixes

--- src/vue-components/popup.vue
+++ src/vue-components/popup.vue
@@ -77,7 +77,7 @@
                      :classes="['alpheios-popup__notifications--lang-switcher']" @change="settingChanged"
                      v-show="data.notification.showLanguageSwitcher"></setting>
         </div>
-        <lookup :uiController="uiController" :preferredLanguage="data.settings.preferredLanguage :lexicons="data.resourceSettings.lexicons"></lookup>
+        <lookup :uiController="uiController" :preferredLanguage="data.settings.preferredLanguage" :lexicons="data.resourceSettings.lexicons"></lookup>
     </div>
 </template>
 <script>
