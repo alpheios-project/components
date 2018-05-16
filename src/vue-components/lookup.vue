5971583c767274a606b12c9507ed21683779132a
irina060981 <irina060981@yandex.ru>
2018-05-16 14:02:42 +1000

    fixes

--- src/vue-components/lookup.vue
+++ src/vue-components/lookup.vue
@@ -13,7 +13,7 @@
     <div class="alpheios-lookup__settings">
       <a class="alpheios-lookup__settings-link" @click="switchLookupSettings">{{ labelSettings }}</a>
       <div class="alpheios-lookup__settings-items" v-show="showLanguageSettings">
-        <alph-setting :data="currentLanguage" @change="settingChanged" :classes="['alpheios-panel__options-item']"></alph-setting>
+        <alph-setting v-if="currentLanguage" :data="currentLanguage" @change="settingChanged" :classes="['alpheios-panel__options-item']"></alph-setting>
 
         <alph-setting :data="languageSetting" @change="resourceSettingChanged" :classes="['alpheios-panel__options-item']"
                   :key="languageSetting.name"
