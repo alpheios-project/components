5383b8a27b29ca6fc7e9accd73f8ef914aa7f1b6
irina060981 <irina060981@yandex.ru>
2018-05-16 20:03:50 +1000

    fix

--- src/vue-components/lookup.vue
+++ src/vue-components/lookup.vue
@@ -136,7 +136,7 @@
       resourceSettingChanged: function (name, value) {
         console.log('***************************** resourceSettingChanged 0', name, value)
         console.log('***************************** resourceSettingChanged 0', this.resourceOptions.items.lexicons)
-        let changedLexiconIndex = this.resourceOptions.items.lexicons.findIndex(item => item.name = name)
+        let changedLexiconIndex = this.resourceOptions.items.lexicons.findIndex(item => item.name === name)
         console.log('***************************** resourceSettingChanged 1', changedLexiconIndex)
         let valuesLexicons = this.resourceOptions.items.lexicons[changedLexiconIndex].values.filter(item => value.indexOf(item.text) > -1)
         console.log('***************************** resourceSettingChanged 2', this.resourceOptions.items.lexicons[changedLexiconIndex].values)
