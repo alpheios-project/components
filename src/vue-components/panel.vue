2fcb84b86010ec05518a198c4ef3b960f624bfde
irina060981 <irina060981@yandex.ru>
2018-05-17 12:46:58 +1000

    fix props reskin

--- src/vue-components/panel.vue
+++ src/vue-components/panel.vue
@@ -120,7 +120,7 @@
                 </div>
             </div>
             <div v-show="data.tabs.options" class="alpheios-panel__tab-panel">
-                <reskin-font-color show-title="true" labelText="Skin options"></reskin-font-color>
+                <reskin-font-color labelText="Skin options"></reskin-font-color>
                 <setting :data="data.settings.preferredLanguage" @change="settingChanged"
                          :classes="['alpheios-panel__options-item']"></setting>
                 <setting :data="data.settings.panelPosition" @change="settingChanged"
