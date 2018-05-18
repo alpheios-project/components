573b7a0e4dc2ed759b386be2bd37d0059377ae06
Kirlat <kirlat@yula.media>
2018-05-18 14:39:34 +0400

    Updated to listen to touch events (for iOS and Android devices). This should be a fix for alpheios-project/pwa-prototype#13

--- src/lib/selection/media/media-selector.js
+++ src/lib/selection/media/media-selector.js
@@ -27,18 +27,13 @@ export default class MediaSelector {
   }
 
   /**
-   * Returns a language code of a selection target. If language cannot be determined, defaultLanguageCode will be used instead.
+   * Returns a language ID of a selection target. If language cannot be determined,
+   * a language from defaultLanguageCode will be used.
    * @param {string} defaultLanguageCode - A default language code that will be used if language cannot be determined.
-   * @return {string} A language code of a selection
+   * @return {symbol} A language ID of a selection
    */
-  getLanguageCode (defaultLanguageCode) {
+  getLanguageID (defaultLanguageCode) {
     let code = this.getLanguageCodeFromSource() || defaultLanguageCode
-    let langId = LanguageModelFactory.getLanguageIdFromCode(code)
-    if (langId) {
-      code = LanguageModelFactory.getLanguageCodeFromId(langId)
-    } else {
-      code = defaultLanguageCode
-    }
-    return code
+    return LanguageModelFactory.getLanguageIdFromCode(code)
   }
 }
