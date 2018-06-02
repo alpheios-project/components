3e4dccc83338543d26993f04f0ce8a4af3d40a80
irina060981 <irina060981@yandex.ru>
2018-06-02 21:54:54 +1000

    change lookup testing

--- src/vue-components/lookup.vue
+++ src/vue-components/lookup.vue
@@ -127,7 +127,6 @@
 
         this.resourceOptions.items[keyinfo.setting].filter((f) => f.name === name).forEach((f) => { f.setTextValue(value) })
 
-        console.log('***************** resourceSettingChange 1 value', this.resourceOptions.items[keyinfo.setting][0].currentValue())
         console.log('***************** resourceSettingChange 2 textValue', this.resourceOptions.items[keyinfo.setting][0].currentTextValue())
       },
 
