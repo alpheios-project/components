dce5fbd641c53c8094b09e8ed3b4ae1abc5fb22d
Irina Sklyarova <irina060981@yandex.ru>
2019-05-13 13:46:05 +1000

    hide moreResults option fro word usage examples filter

--- dist/alpheios-components.js
+++ dist/alpheios-components.js
@@ -17623,6 +17623,7 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
+//
 
 
 
@@ -17645,7 +17646,7 @@ __webpack_require__.r(__webpack_exports__);
       lastTextWorksList: [],
       typeFiltersList: [
         { value: 'noFilters', label: this.l10n.getText('WORDUSAGE_FILTERS_TYPE_NO_FILTERS') },
-        { value: 'moreResults', label: this.l10n.getText('WORDUSAGE_FILTERS_TYPE_MORE_RESULTS'), disabled: true },
+        { value: 'moreResults', label: this.l10n.getText('WORDUSAGE_FILTERS_TYPE_MORE_RESULTS'), disabled: true, skip: true },
         { value: 'filterCurrentResults', label: this.l10n.getText('WORDUSAGE_FILTERS_TYPE_FILTER_CURRENT_RESULTS'), disabled: true }
       ],
       disabledButton: false
@@ -17662,8 +17663,7 @@ __webpack_require__.r(__webpack_exports__);
           this.lastAuthorsList = []
           this.lastTextWorksList = []
           this.typeFilter = 'noFilters'
-          this.setDisabledToType('moreResults')
-          this.setDisabledToType('filterCurrentResults')
+          this.setDisabledToType(['moreResults', 'filterCurrentResults'])
         } else {
           this.lastAuthorsList = this.app.wordUsageExamples.wordUsageExamples
             .filter(wordUsageExampleItem => wordUsageExampleItem.author)
@@ -17676,14 +17676,12 @@ __webpack_require__.r(__webpack_exports__);
             .filter((item, pos, self) => item && self.indexOf(item) == pos)
             .slice()
 
-          this.removeDisabledFromTypeFilters()
           this.typeFilter = 'moreResults'
-          this.setDisabledToType('noFilters')
+          this.setDisabledToType(['noFilters'])
         }
       } else if (!this.$store.state.app.wordUsageExamplesReady && !this.app.homonym) {
-        this.removeDisabledFromTypeFilters()
         this.typeFilter = 'noFilters'
-        this.setDisabledToType('filterCurrentResults')
+        this.setDisabledToType(['moreResults', 'filterCurrentResults'])
         this.selectedAuthor = null
         this.selectedTextWork = null
       }
@@ -17699,14 +17697,14 @@ __webpack_require__.r(__webpack_exports__);
     }
   },
   methods: {
-    removeDisabledFromTypeFilters () {
+    setDisabledToType (typeValues) {
       this.typeFiltersList.forEach(item => {
-        item.disabled = false
+        if (typeValues.indexOf(item.value) > -1) {
+          item.disabled = true
+        } else {
+          item.disabled = false
+        }
       })
-    },
-    setDisabledToType (typeValue) {
-      this.removeDisabledFromTypeFilters()
-      this.typeFiltersList.find(item => item.value === typeValue).disabled = true
     },
     async getResults () {
       if (this.typeFilter === 'noFilters') {
@@ -17716,11 +17714,10 @@ __webpack_require__.r(__webpack_exports__);
 
         await this.getResultsNoFilters()
 
-        this.removeDisabledFromTypeFilters()
         this.clearFilter('author')
         this.lastAuthorID = null
-        this.typeFilter = 'moreResults'
-        this.setDisabledToType('noFilters')
+        this.typeFilter = 'filterCurrentResults'
+        this.setDisabledToType(['noFilters'])
 
         this.disabledButton = false
         
@@ -17729,7 +17726,7 @@ __webpack_require__.r(__webpack_exports__);
         this.$emit('getMoreResults', this.selectedAuthor, this.selectedTextWork)
         await this.getResultsWithFilters()
 
-        this.setDisabledToType('filterCurrentResults')
+        this.setDisabledToType(['filterCurrentResults'])
         this.lastAuthorID = this.selectedAuthor ? this.selectedAuthor.ID : null
 
         this.disabledButton = false
@@ -26873,47 +26870,49 @@ var render = function() {
       "div",
       { staticClass: "alpheios-word-usage-header-select-type-filters-block" },
       _vm._l(_vm.typeFiltersList, function(typeFilterItem) {
-        return _c(
-          "div",
-          {
-            key: typeFilterItem.value,
-            staticClass: "alpheios-word-usage-header-select-type-filter",
-            class: {
-              "alpheios-word-usage-header-select-type-filter-disabled":
-                typeFilterItem.disabled === true
-            }
-          },
-          [
-            _c("input", {
-              directives: [
-                {
-                  name: "model",
-                  rawName: "v-model",
-                  value: _vm.typeFilter,
-                  expression: "typeFilter"
+        return !typeFilterItem.skip
+          ? _c(
+              "div",
+              {
+                key: typeFilterItem.value,
+                staticClass: "alpheios-word-usage-header-select-type-filter",
+                class: {
+                  "alpheios-word-usage-header-select-type-filter-disabled":
+                    typeFilterItem.disabled === true
                 }
-              ],
-              attrs: {
-                type: "radio",
-                id: typeFilterItem.value,
-                disabled: typeFilterItem.disabled === true
               },
-              domProps: {
-                value: typeFilterItem.value,
-                checked: _vm._q(_vm.typeFilter, typeFilterItem.value)
-              },
-              on: {
-                change: function($event) {
-                  _vm.typeFilter = typeFilterItem.value
-                }
-              }
-            }),
-            _vm._v(" "),
-            _c("label", { attrs: { for: typeFilterItem.value } }, [
-              _vm._v(_vm._s(typeFilterItem.label))
-            ])
-          ]
-        )
+              [
+                _c("input", {
+                  directives: [
+                    {
+                      name: "model",
+                      rawName: "v-model",
+                      value: _vm.typeFilter,
+                      expression: "typeFilter"
+                    }
+                  ],
+                  attrs: {
+                    type: "radio",
+                    id: typeFilterItem.value,
+                    disabled: typeFilterItem.disabled === true
+                  },
+                  domProps: {
+                    value: typeFilterItem.value,
+                    checked: _vm._q(_vm.typeFilter, typeFilterItem.value)
+                  },
+                  on: {
+                    change: function($event) {
+                      _vm.typeFilter = typeFilterItem.value
+                    }
+                  }
+                }),
+                _vm._v(" "),
+                _c("label", { attrs: { for: typeFilterItem.value } }, [
+                  _vm._v(_vm._s(typeFilterItem.label))
+                ])
+              ]
+            )
+          : _vm._e()
       }),
       0
     ),
@@ -39623,7 +39622,7 @@ __webpack_require__.r(__webpack_exports__);
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
-/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "Store", function() { return Store; });
+/* WEBPACK VAR INJECTION */(function(global) {/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "Store", function() { return Store; });
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "install", function() { return install; });
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "mapState", function() { return mapState; });
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "mapMutations", function() { return mapMutations; });
@@ -39631,7 +39630,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "mapActions", function() { return mapActions; });
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "createNamespacedHelpers", function() { return createNamespacedHelpers; });
 /**
- * vuex v3.1.0
+ * vuex v3.1.1
  * (c) 2019 Evan You
  * @license MIT
  */
@@ -39671,9 +39670,12 @@ function applyMixin (Vue) {
   }
 }
 
-var devtoolHook =
-  typeof window !== 'undefined' &&
-  window.__VUE_DEVTOOLS_GLOBAL_HOOK__;
+var target = typeof window !== 'undefined'
+  ? window
+  : typeof global !== 'undefined'
+    ? global
+    : {};
+var devtoolHook = target.__VUE_DEVTOOLS_GLOBAL_HOOK__;
 
 function devtoolPlugin (store) {
   if (!devtoolHook) { return }
@@ -39717,6 +39719,12 @@ function isPromise (val) {
 
 function assert (condition, msg) {
   if (!condition) { throw new Error(("[vuex] " + msg)) }
+}
+
+function partial (fn, arg) {
+  return function () {
+    return fn(arg)
+  }
 }
 
 // Base data struct for store's module, package with some attribute and method
@@ -40180,7 +40188,9 @@ function resetStoreVM (store, state, hot) {
   var computed = {};
   forEachValue(wrappedGetters, function (fn, key) {
     // use computed to leverage its lazy-caching mechanism
-    computed[key] = function () { return fn(store); };
+    // direct inline function use will lead to closure preserving oldVm.
+    // using partial to return function with only arguments preserved in closure enviroment.
+    computed[key] = partial(fn, store);
     Object.defineProperty(store.getters, key, {
       get: function () { return store._vm[key]; },
       enumerable: true // for local getters
@@ -40619,7 +40629,7 @@ function getModuleByNamespace (store, helper, namespace) {
 var index_esm = {
   Store: Store,
   install: install,
-  version: '3.1.0',
+  version: '3.1.1',
   mapState: mapState,
   mapMutations: mapMutations,
   mapGetters: mapGetters,
@@ -40630,6 +40640,7 @@ var index_esm = {
 /* harmony default export */ __webpack_exports__["default"] = (index_esm);
 
 
+/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! ./../../webpack/buildin/global.js */ "../node_modules/webpack/buildin/global.js")))
 
 /***/ }),
 
@@ -40736,7 +40747,7 @@ __webpack_require__.r(__webpack_exports__);
               attrs: Object.assign({"viewBox":"0 0 20 20","xmlns":"http://www.w3.org/2000/svg"}, attrs),
               ...rest,
             },
-            children.concat([_c('path',{attrs:{"fill":"none","stroke-width":"1.6","d":"M14 1l-8 9 8 9"}})])
+            children.concat([_c('path',{attrs:{"fill":"none","d":"M14 1l-8 9 8 9"}})])
           )
         }
       });
@@ -40776,7 +40787,7 @@ __webpack_require__.r(__webpack_exports__);
               attrs: Object.assign({"viewBox":"0 0 20 20","xmlns":"http://www.w3.org/2000/svg"}, attrs),
               ...rest,
             },
-            children.concat([_c('path',{attrs:{"fill":"none","stroke-width":"1.6","d":"M6 1l8 9-8 9"}})])
+            children.concat([_c('path',{attrs:{"fill":"none","d":"M6 1l8 9-8 9"}})])
           )
         }
       });
@@ -41416,7 +41427,7 @@ __webpack_require__.r(__webpack_exports__);
               attrs: Object.assign({"viewBox":"0 0 20 20","xmlns":"http://www.w3.org/2000/svg"}, attrs),
               ...rest,
             },
-            children.concat([_c('path',{attrs:{"stroke-width":"0","d":"M12.13 11.59c-.16 1.25-1.78 2.53-3.03 2.57-2.93.04.79-4.7-.36-5.79.56-.21 1.88-.54 1.88.44 0 .82-.5 1.74-.74 2.51-1.22 3.84 2.25-.17 2.26-.14.02.03.02.17-.01.41-.05.36.03-.24 0 0zm-.57-5.92c0 1-2.2 1.48-2.2.36 0-1.03 2.2-1.49 2.2-.36z"}}),_c('circle',{attrs:{"fill":"none","stroke-width":"1.1","cx":"10","cy":"10","r":"9"}})])
+            children.concat([_c('path',{attrs:{"d":"M12.13 11.59c-.16 1.25-1.78 2.53-3.03 2.57-2.93.04.79-4.7-.36-5.79.56-.21 1.88-.54 1.88.44 0 .82-.5 1.74-.74 2.51-1.22 3.84 2.25-.17 2.26-.14.02.03.02.17-.01.41-.05.36.03-.24 0 0zm-.57-5.92c0 1-2.2 1.48-2.2.36 0-1.03 2.2-1.49 2.2-.36z"}}),_c('circle',{attrs:{"fill":"none","cx":"10","cy":"10","r":"9"}})])
           )
         }
       });
@@ -41696,7 +41707,7 @@ __webpack_require__.r(__webpack_exports__);
               attrs: Object.assign({"viewBox":"0 0 20 20","xmlns":"http://www.w3.org/2000/svg"}, attrs),
               ...rest,
             },
-            children.concat([_c('circle',{attrs:{"fill":"none","stroke-width":"1.1","cx":"10","cy":"10","r":"9"}}),_c('path',{attrs:{"stroke-width":"0","d":"M9 4h1v7H9z"}}),_c('path',{attrs:{"fill":"none","stroke-width":"1.1","d":"M13.018 14.197l-3.573-3.572"}})])
+            children.concat([_c('circle',{attrs:{"fill":"none","cx":"10","cy":"10","r":"9"}}),_c('path',{attrs:{"d":"M9 4h1v7H9z"}}),_c('path',{attrs:{"fill":"none","d":"M13.018 14.197l-3.573-3.572"}})])
           )
         }
       });
